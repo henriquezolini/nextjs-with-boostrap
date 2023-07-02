ba3e0dd6a0fd5f6dfce1ae63d8cde3381416c7fb
Henrique Zolini <henriquezolini@gmail.com>
2023-07-02 09:45:25 -0300

    Commit inicial

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
-import '@/styles/globals.css'
+import { useEffect } from "react";
+import "@/styles/index.scss";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+  return <Component {...pageProps} />;
 }
